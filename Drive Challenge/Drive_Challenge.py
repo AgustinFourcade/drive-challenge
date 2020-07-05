d7b627f6c099381ef517f07cc804735caa966f05
Agustin <agussfourcade@gmail.com>
2020-07-05 15:56:00 -0300

    V2

--- Drive Challenge/Drive_Challenge.py
+++ Drive Challenge/Drive_Challenge.py
@@ -12,7 +12,8 @@ from email.mime.base import MIMEBase
 from email.encoders import encode_base64
 import mysql.connector
 
-#Creamos la conexion con la base de datos
+
+# Creamos la conexion con la base de datos
 conexionDB = mysql.connector.connect(
     host = "127.0.0.1",
     user = "root",
@@ -20,18 +21,18 @@ conexionDB = mysql.connector.connect(
 )
 
 try:
-    #Intentamos ingresar a la base de datos DriveDB
+    # Intentamos ingresar a la base de datos DriveDB
     try:
-        cursor = conexionDB.cursor(buffered=True) #El cursor permite enviar y recibir datos
+        cursor = conexionDB.cursor(buffered=True) # El cursor permite enviar y recibir datos
 
-        sqlcnx = cursor.execute("use DriveDB") #Intentamos usar la base de datos (si no existe, entra al catch)
+        sqlcnx = cursor.execute("use DriveDB") # Intentamos usar la base de datos (si no existe, entra al catch)
 
         cursor.execute(sqlcnx)
 
-        print ("Conexion realizada correctamente!")
+        print ("Conexion realizada a la base de datos correctamente!")
         print (" ")
 
-    #En el caso de que la base de datos no exista, la creamos
+    # En el caso de que la base de datos no exista, la creamos
     except mysql.connector.DatabaseError:
 
         print ("La base de datos 'DriveDB' no existe. Se creará automaticamente.")
@@ -46,53 +47,51 @@ try:
         print ("La base de datos 'DriveDB' se creo correctamente!")
         print (" ")
 
-    #Intentamos utilizar la tabla 'Usuarios'
+    # Intentamos utilizar la tabla 'Usuarios'
     try:
 
-        sqlTabla = cursor.execute("SELECT * from usuarios") #Intentamos usar la base de datos (si no existe, entra al catch)
+        sqlTabla = cursor.execute("SELECT * from usuarios") # Intentamos usar la base de datos (si no existe, entra al catch)
+        sqlTabla1 = cursor.execute("SELECT * from historial_publicos") # Intentamos usar la base de datos (si no existe, entra al catch)
 
         cursor.execute(sqlTabla)
+        cursor.execute(sqlTabla1)
 
-    #En el caso de que la tabla 'Usuarios' no exista, la creamos
+    # En el caso de que la tabla 'Usuarios' no exista, la creamos
     except mysql.connector.DatabaseError:
 
-        print ("La tabla 'Usuarios' no existe. Se creará automaticamente.")
+        print ("La tabla 'Usuarios' o 'Historial_publicos' no existe. Se creará automaticamente.")
         print (" ")
         print ("Creando...")
         print (" ")
     
-        sqltbl = cursor.execute("CREATE TABLE `drivedb`.`usuarios` (`idusuarios` INT NOT NULL AUTO_INCREMENT, `owner` VARCHAR(45) NULL, `nombre_archivo` VARCHAR(45) NULL, `extension` VARCHAR(45) NULL, `visibilidad` VARCHAR(45) NULL, `fecha_modificacion` DATE NULL, PRIMARY KEY (`idusuarios`));")
-    
-        cursor.execute(sqltbl)
+        sqltbl1 = cursor.execute("CREATE TABLE `drivedb`.`usuarios` (`idusuarios` INT NOT NULL AUTO_INCREMENT, `owner` VARCHAR(45) NULL, `nombre_archivo` VARCHAR(45) NULL, `extension` VARCHAR(45) NULL, `visibilidad` VARCHAR(45) NULL, `fecha_modificacion` DATE NULL, PRIMARY KEY (`idusuarios`));")
+        sqltbl2 = cursor.execute("CREATE TABLE `drivedb`.`historial_publicos` (`idhistorial_publicos` INT NOT NULL AUTO_INCREMENT, `owner` VARCHAR(45) NULL, `nombre_archivo` VARCHAR(45) NULL, `extension` VARCHAR(45) NULL, `visibilidad` VARCHAR(45) NULL, `fecha_modificacion` DATE NULL, PRIMARY KEY (`idhistorial_publicos`));")
 
-        print ("La tabla 'Usuarios' se creo correctamente!")
+        cursor.execute(sqltbl1)
+        cursor.execute(sqltbl2)
+
+
+        print ("La tabla 'Usuarios' y 'Historail_publicos' se creo correctamente!")
         print (" ")
 
 except exception:
     print ("Error desconocido. Consulte con un administrador.")
     print (" ")
 
-#FINALIZA la conexion y creacion de tablas
+# FINALIZA la conexion y creacion de tablas
 
+# Iniciamos con API GDrive V3
 
-
-#Iniciamos
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+# Apuntamos a la posibilidad de leer y escribir archivos
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # Se utiliza un token.pickle que almacena el acceso del usuario. refrescar el token significa tener que validar el usuario nuevamente.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+    # Si las credenciales no son validas, hacemos que se valide el usuario
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -100,69 +99,92 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        # Guardamos las credenciales para el proximo uso
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('drive', 'v3', credentials=creds)
 
-    # Call the Drive v3 API
+    # Utilizando la API GDrive V3
     results = service.files().list(
-        pageSize=1000, fields="nextPageToken, files(permissionIds, name, fileExtension, owners, modifiedTime)").execute() #Busca los primeros 1000 archivos en nuestro Drive
+        pageSize=1000, fields="nextPageToken, files(permissionIds, name, fileExtension, owners, modifiedTime)").execute() # Busca los primeros 1000 archivos en nuestro Drive
     items = results.get('files', [])
 
     if not items:
-        print('No files found.')
+        print('No files found.') # Si nuestro Drive esta vacio, mostramos un mensaje
 
     else:
+        # Limpiamos la base de datos
         sql = "DELETE From usuarios where idusuarios > 0;"
         sql2 = "ALTER TABLE usuarios AUTO_INCREMENT = 1;"
         cursor.execute(sql)
         cursor.execute(sql2)
+        sql3 = "DELETE From historial_publicos where idhistorial_publicos > 0;"
+        sql4 = "ALTER TABLE historial_publicos AUTO_INCREMENT = 1;"
+        cursor.execute(sql3)
+        cursor.execute(sql4)
         conexionDB.commit()
 
-        print('Files:')
-        print(' ')
-
+        # Almacenamos todos los archivos que se encuentren con su metadata
         for item in items:
 
             try:
-
                 visibilidad = len((item['permissionIds']))
                 nombre = format(item['name'])
                 extension = item['fileExtension']
                 owner = format(item['owners'][0]['emailAddress'])
                 fecha_modificacion = format(item["modifiedTime"])
 
+                # Cambiamos el formato de la fecha de modificacion para que MySQL la acepte (YYYY-mm-dd)
                 mytime = fecha_modificacion
                 myTime = datetime.strptime(mytime, "%Y-%m-%dT%H:%M:%S.%fZ")
                 myFormat = "%Y-%m-%d"
                 nFecha_Modificacion = (myTime.strftime(myFormat))
 
-                print (visibilidad)
-                print (nombre)
-                print (extension)
-                print (owner)
-                print (nFecha_Modificacion)
-                print (" ")
-
+                # Si el archivo es privado, lo guardamos como tal
                 if visibilidad < 2:
-                    sql = "INSERT INTO usuarios (owner, nombre_archivo, extension, visibilidad, fecha_modificacion) VALUES (%s, %s, %s, %s, %s)"
-                    val = (owner, nombre, extension, "Privado", nFecha_Modificacion)
-                    cursor.execute(sql, val)
+                    sqlUsuarios = "INSERT INTO usuarios (owner, nombre_archivo, extension, visibilidad, fecha_modificacion) VALUES (%s, %s, %s, %s, %s)"
+
+                    valores = (owner, nombre, extension, "Privado", nFecha_Modificacion)
+
+                    cursor.execute(sqlUsuarios, valores)
+
                     conexionDB.commit()
+
                 else:
-                    sql = "INSERT INTO usuarios (owner, nombre_archivo, extension, visibilidad, fecha_modificacion) VALUES (%s, %s, %s, %s, %s)"
-                    val = (owner, nombre, extension, "Publico", nFecha_Modificacion)
-                    cursor.execute(sql, val)
+                    valoresPublico = (owner, nombre, extension, "Publico", nFecha_Modificacion)
+                    valoresPriv = (owner, nombre, extension, "Privado", nFecha_Modificacion)
+
+                    sqlUsuarios = "INSERT INTO usuarios (owner, nombre_archivo, extension, visibilidad, fecha_modificacion) VALUES (%s, %s, %s, %s, %s)"
+                    cursor.execute(sqlUsuarios, valoresPriv)
+
+                    # Guardamos el historial historico de los archivos que fueron publicos
+                    sqlHistorial = "INSERT INTO historial_publicos (owner, nombre_archivo, extension, visibilidad, fecha_modificacion) VALUES (%s, %s, %s, %s, %s)"
+                    cursor.execute(sqlHistorial, valoresPublico)
+
                     conexionDB.commit()
+
+                    # Enviamos correo al owner del archivo que cambiamos de publico a privado.
+                    mensaje = 'Se notifica de la actualizacion de un archivo de su Google Drive. Este mensaje se envia desde el challenge Gmail desarrollado en Python, es meramente de prueba. Agustin Fourcade - 2020'
+                   
+                    server = smtplib.SMTP('smtp.gmail.com', 587)
+                    server.starttls()
+                    server.login('drivechallengemeli@gmail.com', 'drivechallenge') 
+
+                    server.sendmail('shockh13@gmail.com', owner, mensaje)
+
+                    server.quit()
+
+                    print (f"Se envio el correo electronico a: {owner}")
+                    print (" ")
 
             except:
-                msjError = ("Hay Error")
+                msjError = ("Hay Error") # Si se encuentra un error con los archivos o sus metadatos obtenemos un error
 
         else:
-            print ("Se cargaron los prouctos a la base de datos correctamente!")
+            print ("Se cargaron los prouctos a la base de datos correctamente!") # Si todo sale bien, mostramos un mensaje
             print (" ")
+
 
 if __name__ == '__main__':
     main()
